2fa4241e61c42a5b1900306f5bc242375871f11b
Abdul Abbou <53510613+aabb1998@users.noreply.github.com>
2022-01-04 23:49:26 +1100

    Added live market prices.

---
+++ client/src/Components/LiveMarket/CoinData.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import "./LiveMarket.css";
+
+const CoinData = (data, index) => {
+	return (
+		<tr>
+			<td>{data.data.market_cap_rank}</td>
+			<td>
+				<img className="coin__image" src={data.data.image} alt="" />
+			</td>
+			<td>{data.data.name}</td>
+			<td>US ${data.data.current_price.toLocaleString()}</td>
+			<td
+				style={
+					data.data.price_change_24h < 0
+						? { color: "red" }
+						: { color: "green" }
+				}
+			>
+				{parseFloat(data.data.price_change_percentage_24h).toFixed(2)} %
+			</td>
+			<td>US ${data.data.high_24h}</td>
+		</tr>
+	);
+};
+
+export default CoinData;
--- client/src/Components/LiveMarket/LiveMarket.jsx
+++ client/src/Components/LiveMarket/LiveMarket.jsx
@@ -1,67 +1,79 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
-import { useState } from 'react';
-import './LiveMarket.css';
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useState } from "react";
+import "./LiveMarket.css";
+import CoinData from "./CoinData";
 
 const LiveMarket = () => {
-  const [coinData, setCoinData] = useState([]);
+	const [coinData, setCoinData] = useState([]);
+	const [coinDataAmount, setCoinDataAmount] = useState(5);
 
-  const fetchCurrentMarket = () => {
-    const options = {
-      method: 'GET',
-      url: 'https://coingecko.p.rapidapi.com/coins/markets',
-      params: {
-        vs_currency: 'usd',
-        page: '1',
-        per_page: '100',
-        order: 'market_cap_desc',
-      },
-      headers: {
-        'x-rapidapi-host': 'coingecko.p.rapidapi.com',
-        'x-rapidapi-key': 'b33f6a7648mshe938a076e257b22p1049f7jsne20c6769b910',
-      },
-    };
+	const fetchCurrentMarket = () => {
+		const options = {
+			method: "GET",
+			url: "https://coingecko.p.rapidapi.com/coins/markets",
+			params: {
+				vs_currency: "usd",
+				page: "1",
+				per_page: "100",
+				order: "market_cap_desc",
+			},
+			headers: {
+				"x-rapidapi-host": "coingecko.p.rapidapi.com",
+				"x-rapidapi-key":
+					"b33f6a7648mshe938a076e257b22p1049f7jsne20c6769b910",
+			},
+		};
 
-    axios
-      .request(options)
-      .then(function (response) {
-        const data = response.data;
-        setCoinData(data.slice(0, 5));
-        console.log(coinData);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  };
+		axios
+			.request(options)
+			.then(function (response) {
+				const data = response.data;
+				setCoinData(data.slice(0, coinDataAmount));
+				console.log(coinData);
+			})
+			.catch(function (error) {
+				console.error(error);
+			});
+	};
 
-  useEffect(() => {
-    fetchCurrentMarket();
-  }, []);
+	const handleLoad = () => {
+		setCoinDataAmount(coinDataAmount + 5);
+	};
 
-  return (
-    <div className="liveMarket">
-      <div className="liveMarket__section">
-        <div className="liveMarket__header">
-          <h4>#</h4>
-          <h4>Name</h4>
-          <h4>Price</h4>
-          <h4>Change</h4>
-          <h4>24H High</h4>
-        </div>
-        {coinData.map((coin) => {
-          return (
-            <div className="liveMarket__data-coin">
-              <h4 key={coin.market_cap_rank}>{coin.market_cap_rank}</h4>
-              <h4 key={coin.market_cap_rank}>{coin.id}</h4>
-              <h4 key={coin.market_cap_rank}>{coin.current_price}</h4>
-              <h4 key={coin.market_cap_rank}>{coin.price_change_24h}</h4>
-              <h4 key={coin.market_cap_rank}>{coin.high_24h}</h4>
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+	useEffect(() => {
+		fetchCurrentMarket();
+	}, [coinDataAmount]);
+
+	useEffect(() => {
+		fetchCurrentMarket();
+	}, []);
+
+	return (
+		<div className="liveMarket">
+			<div className="liveMarket__section">
+				<div className="liveMarket__header">
+					<table id="data">
+						<tr>
+							<th>#</th>
+							<th></th>
+							<th>Name</th>
+							<th>Price</th>
+							<th>Change</th>
+							<th>24H High</th>
+						</tr>
+						{coinData &&
+							coinData.map((coin, index) => (
+								<CoinData data={coin} index={index} />
+							))}
+					</table>
+				</div>
+				<div className="data__load">
+					<button onClick={handleLoad}>Load More</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default LiveMarket;
