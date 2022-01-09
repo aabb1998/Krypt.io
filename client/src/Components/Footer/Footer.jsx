9628c723442c6ad906dde1255274f575e8b0b0e6
Abdul Abbou <53510613+aabb1998@users.noreply.github.com>
2022-01-09 20:43:21 +1100

    CSS fixes.

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,33 +1,51 @@
-import React from 'react';
-import './Footer.css';
-import logo from '../../assets/logo.png';
+import React from "react";
+import "./Footer.css";
+import logo from "../../assets/logo.png";
+import { useUserAuth } from "../../Context/UserAuthContext";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return (
-    <div className="footer__section">
-      <div className="footer__section-container">
-        <div className="footer__section-left">
-          <p>Take your crypto to the next level with pureCrypto.io</p>
-          <button className="footer__button-login">Log In</button>
-          <button className="footer__button-signup">Sign Up</button>
-        </div>
-        <div className="footer__section-right">
-          <nav>
-            <ul>
-              <li>Cryptocurrencies</li>
-              <li>Exchanges</li>
-              <li>News</li>
-              <li>Calendars</li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-      <div className="footer__section-logo">
-        <img src={logo} alt="" />
-        <p>© 2021 pureCrypto.io. All rights reserved</p>
-      </div>
-    </div>
-  );
+	const { user } = useUserAuth();
+
+	return (
+		<div className="footer__section">
+			<div className="footer__section-container">
+				<div className="footer__section-left">
+					<p>Take your crypto to the next level with pureCrypto.io</p>
+					<div
+						style={user && { display: "none" }}
+						className="footer__section-button"
+					>
+						<Link to="/login">
+							<button className="footer__button-login">
+								Log in
+							</button>
+						</Link>
+
+						<Link to="/signup">
+							<button className="footer__button-signup">
+								Sign Up
+							</button>
+						</Link>
+					</div>
+				</div>
+				<div className="footer__section-right">
+					<nav>
+						<ul>
+							<li>Cryptocurrencies</li>
+							<li>Exchanges</li>
+							<li>News</li>
+							<li>Calendars</li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+			<div className="footer__section-logo">
+				<img src={logo} alt="" />
+				<p>© 2021 pureCrypto.io. All rights reserved</p>
+			</div>
+		</div>
+	);
 };
 
 export default Footer;
--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,75 +1,77 @@
-import React, { useState } from 'react';
-import logo from '../../assets/logo.png';
-import { HiMenuAlt4 } from 'react-icons/hi';
-import { AiOutlineClose } from 'react-icons/ai';
-import { Outlet, Link } from 'react-router-dom';
-import './Navbar.css';
-import App from './../../App';
-import { useUserAuth } from '../../Context/UserAuthContext';
-import { useEffect } from 'react';
-import { BiLogOut } from 'react-icons/bi';
+import React, { useState } from "react";
+import logo from "../../assets/logo.png";
+import { HiMenuAlt4 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+import { Outlet, Link } from "react-router-dom";
+import "./Navbar.css";
+import App from "./../../App";
+import { useUserAuth } from "../../Context/UserAuthContext";
+import { useEffect } from "react";
+import { BiLogOut } from "react-icons/bi";
 
 const NavbarItems = ({ title, classProps }) => (
-  <li className={`cursor-pointer mx-9 nav-links sm:text-[15px] ${classProps}`}>
-    <Link to="">{title}</Link>
-  </li>
+	<li
+		className={`cursor-pointer mx-9 nav-links sm:text-[15px] ${classProps}`}
+	>
+		<Link to="">{title}</Link>
+	</li>
 );
 
 const LogOut = ({ user, handleLogout }) => (
-  <div className="logout__container">
-    <span style={{ fontWeight: '500' }}>{user.displayName}</span>
-    <BiLogOut
-      onClick={handleLogout}
-      style={{ marginLeft: '10px', cursor: 'pointer' }}
-      fontSize={22}
-      color="#3861fb"
-    />
-  </div>
+	<div className="logout__container">
+		<span style={{ fontWeight: "500" }}>{user.displayName}</span>
+		<BiLogOut
+			onClick={handleLogout}
+			style={{ marginLeft: "10px", cursor: "pointer" }}
+			fontSize={22}
+			color="#3861fb"
+		/>
+	</div>
 );
 
 const Navbar = () => {
-  const [toggleMenu, setToggleMenu] = useState(false);
-  const { user, logOut } = useUserAuth();
+	const [toggleMenu, setToggleMenu] = useState(false);
+	const { user, logOut } = useUserAuth();
 
-  const handleLogout = async () => {
-    try {
-      await logOut();
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
+	const handleLogout = async () => {
+		try {
+			await logOut();
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
 
-  return (
-    <nav className="w-full flex md:justify-around justify-between items-center p-4 select-none navbar">
-      <div className="flex-initial justify-center items-center">
-        <Link to="/">
-          <img
-            className="w-32 cursor-pointer select-none"
-            src={logo}
-            alt="pureCrypto.io"
-          />
-        </Link>
-      </div>
-      <ul className="nav__list flex list-none flex-row justify-between items-center p-4">
-        {['Cryptocurrencies', 'Exchanges', 'Prices', 'News'].map(
-          (item, index) => (
-            <NavbarItems key={item + index} title={item} />
-          )
-        )}
-      </ul>
-      {!user && (
-        <div className="button__container">
-          <Link to="/login">
-            <button className=" text-white bg-[#3861FB] py-1.5 px-5 rounded-[8px] cursor-pointer hover:text-black">
-              Log in
-            </button>
-          </Link>
-        </div>
-      )}
+	return (
+		<nav className="w-full flex md:justify-around justify-between items-center p-4 select-none navbar">
+			<div className="flex-initial justify-center items-center">
+				<Link to="/">
+					<img
+						className="w-32 cursor-pointer select-none"
+						src={logo}
+						alt="pureCrypto.io"
+					/>
+				</Link>
+			</div>
+			<ul className="nav__list flex list-none flex-row justify-between items-center p-4">
+				{["Cryptocurrencies", "Exchanges", "Prices", "News"].map(
+					(item, index) => (
+						<NavbarItems key={item + index} title={item} />
+					)
+				)}
+			</ul>
+			{!user && (
+				<div className="button__container">
+					<Link to="/login">
+						<button className=" text-white bg-[#3861FB] py-1.5 px-5 rounded-[8px] cursor-pointer hover:text-black">
+							Log in
+						</button>
+					</Link>
+				</div>
+			)}
 
-      {user && <LogOut user={user} handleLogout={handleLogout} />}
+			{user && <LogOut user={user} handleLogout={handleLogout} />}
 
-      {/* <div className="flex relative">
+			{/* <div className="flex relative">
         {!toggleMenu && (
           <HiMenuAlt4
             fontSize={24}
@@ -80,8 +82,8 @@ const Navbar = () => {
           <AiOutlineClose fontSize={24} className="md:hidden cursor-pointer" />
         )}
       </div> */}
-    </nav>
-  );
+		</nav>
+	);
 };
 
 export default Navbar;
