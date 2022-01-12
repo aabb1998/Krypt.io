b863566d85f51b81a22dd5d14ce91ba079244264
aabb1998 <53510613+aabb1998@users.noreply.github.com>
2022-01-12 23:41:35 +1100

    Fixed dashboard css issues.

--- client/src/Routes/Dashboard/Dashboard.jsx
+++ client/src/Routes/Dashboard/Dashboard.jsx
@@ -1,22 +1,25 @@
-import React from "react";
-import DashboardNavbar from "./DashboardNavbar";
-import LeftMenu from "./LeftMenu/LeftMenu";
-import "./Dashboard.css";
+import React from 'react';
+import DashboardNavbar from './DashboardNavbar';
+import LeftMenu from './LeftMenu/LeftMenu';
+import './Dashboard.css';
+import DashboardMain from './DashboardMain/DashboardMain';
 
 const Dashboard = () => {
-	return (
-		<div className="Dashboard">
-			<div className="dashboard__nav">
-				<DashboardNavbar />
-			</div>
-			<div className="dashboard__main">
-				<div className="dashboard__leftMenu">
-					<LeftMenu />
-				</div>
-				<div className="dashboard__mainContainer"></div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="Dashboard">
+      <div className="dashboard__nav">
+        <DashboardNavbar />
+      </div>
+      <div className="dashboard__main">
+        <div className="dashboard__leftMenu">
+          <LeftMenu />
+        </div>
+        <div className="dashboard__mainContainer">
+          <DashboardMain />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
--- client/src/Routes/Dashboard/LeftMenu/LeftMenu.jsx
+++ client/src/Routes/Dashboard/LeftMenu/LeftMenu.jsx
@@ -1,35 +1,60 @@
-import React from "react";
-import market from "../../../assets/DashboardAssets/market.png";
-import "./LeftMenu.css";
+import React from 'react';
+import market from '../../../assets/DashboardAssets/market.png';
+import dashboard from '../../../assets/DashboardAssets/dashboard.png';
+import analytics from '../../../assets/DashboardAssets/analytics.png';
+import portfolio from '../../../assets/DashboardAssets/portfolio.png';
+import news from '../../../assets/DashboardAssets/news.png';
+import help from '../../../assets/DashboardAssets/help.png';
+import settings from '../../../assets/DashboardAssets/settings.png';
+import logout from '../../../assets/DashboardAssets/logout.png';
+
+import './LeftMenu.css';
 
 const LeftMenu = () => {
-	return (
-		<div className="left__menu">
-			<div className="left__menuLinks">
-				<h5>MENU</h5>
-				<div className="menu__link">
-					<img src={market} alt="" />
-					<h5>MARKET</h5>
-				</div>
-				<div className="menu__link">
-					<img src={market} alt="" />
-					<h5>MARKET</h5>
-				</div>
-				<div className="menu__link">
-					<img src={market} alt="" />
-					<h5>MARKET</h5>
-				</div>
-				<div className="menu__link">
-					<img src={market} alt="" />
-					<h5>MARKET</h5>
-				</div>
-				<div className="menu__link">
-					<img src={market} alt="" />
-					<h5>MARKET</h5>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="left__menu">
+      <div className="left__menuLinks">
+        <h5 className="left__menu-header">MENU</h5>
+        <div className="menu__link">
+          <img src={market} alt="" />
+          <h5>MARKET</h5>
+        </div>
+        <div className="menu__link">
+          <img src={dashboard} alt="" />
+          <h5>DASHBOARD</h5>
+        </div>
+        <div className="menu__link">
+          <img src={analytics} alt="" />
+          <h5>ANALYTICS</h5>
+        </div>
+        <div className="menu__link">
+          <img src={portfolio} alt="" />
+          <h5>MY PORTFOLIO</h5>
+        </div>
+        <div className="menu__link">
+          <img src={news} alt="" />
+          <h5>NEWS</h5>
+        </div>
+      </div>
+      <div className="left__menuLinksOther">
+        <h5 className="left__menu-header">OTHER</h5>
+        <div className="menu__link">
+          <img src={help} alt="" />
+          <h5>HELP</h5>
+        </div>
+        <div className="menu__link">
+          <img src={settings} alt="" />
+          <h5>SETTINGS</h5>
+        </div>
+      </div>
+      <div className="left__menuLogout">
+        <div className="menu__link-logout">
+          <img src={logout} alt="" />
+          <h5>LOG OUT</h5>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LeftMenu;
