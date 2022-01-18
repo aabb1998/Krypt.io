424fb3189e96d9022d3d046a2dfeaf91075177f2
Abdul Abbou <53510613+aabb1998@users.noreply.github.com>
2022-01-19 00:08:11 +1100

    Added profile portfolio tracking.

--- client/src/Routes/Dashboard/DashboardMain/DashboardMain.jsx
+++ client/src/Routes/Dashboard/DashboardMain/DashboardMain.jsx
@@ -9,6 +9,33 @@ const DashboardMain = () => {
 			<div className="dashboard__main-header">
 				<h4>Dashboard</h4>
 				<span>An overview of your portfolio and markets.</span>
+			</div>
+			<div className="dashboard__main-portfolio">
+				<div className="portfolio-value">
+					<div className="portfolio-value__header">
+						<h5>My Portfolio</h5>
+					</div>
+					<div className="portfolio-value__data">
+						<div className="portfolio-value__image">
+							<img
+								src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"
+								alt=""
+							/>
+						</div>
+						<div className="portfolio-value__value">
+							<span>$324,000</span>
+							<p>$42,130.24</p>
+						</div>
+						<div className="portfolio-value__change">
+							<p style={{ color: "greenyellow" }}>4.32%</p>
+						</div>
+					</div>
+					<div className="portfolio-value__currency">
+						<p>USD</p>
+					</div>
+				</div>
+			</div>
+			<div className="dashboard__main-marketPrices">
 				<Prices />
 			</div>
 		</div>
